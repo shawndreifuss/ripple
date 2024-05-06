42b604bf1e1f1681e57eba13af53968dd01c81ec
Shawn Dreifuss <shawndreifuss@icloud.com>
2024-05-05 21:18:42 -0700

    added final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
 import { DM_Sans } from 'next/font/google'
 import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
-import { dark } from '@clerk/themes'
 import { ThemeProvider } from '@/providers/theme-provider'
 import ModalProvider from '@/providers/modal-provider'
 import { Toaster } from '@/components/ui/toaster'
@@ -11,8 +9,11 @@ import { Toaster as SonnarToaster } from '@/components/ui/sonner'
 const font = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Plura',
+  title: 'Ripple',
   description: 'All in one Agency Solution',
+  viewport: 'width=device-width, initial-scale=1',
+  keywords: ['Ripple', 'Agency', 'Solution'],
+
 }
 
 export default function RootLayout({
@@ -25,6 +26,8 @@ export default function RootLayout({
       lang="en"
       suppressHydrationWarning
     >
+      <meta name="description" content="Built with Next.js and TypeScript" />
+      <link rel="icon" href="/assets/logo-icon.png" />
       <body className={font.className}>
         <ThemeProvider
           attribute="class"
