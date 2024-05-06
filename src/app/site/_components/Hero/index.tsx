0d7b3b1f84a2fadcb340226cc5dde2385752498b
Shawn Dreifuss <shawndreifuss@icloud.com>
2024-05-05 20:38:59 -0700

    removed comments

--- src/app/site/_components/Hero/index.tsx
+++ src/app/site/_components/Hero/index.tsx
@@ -1,10 +1,8 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 
 const Hero = () => {
-  const [email, setEmail] = useState("");
 
   
 
