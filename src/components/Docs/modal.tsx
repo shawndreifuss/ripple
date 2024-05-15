9565c5b290d9700881bb5c4949b8e87c6078aa72
Shawn Dreifuss <shawndreifuss@icloud.com>
2024-05-14 22:23:27 -0700

    added video guide

--- src/components/Docs/modal.tsx
+++ src/components/Docs/modal.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -7,23 +7,38 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import Image from "next/image"
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
 
 export function Modal() {
   return (
-    <div className="absolute w-full h-full z-40 opacity-0 hover:bg-black  hover:opacity-20">
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline" className="w-full h-full z-50  overflow-hidden hover:bg-black  hover:opacity-20"></Button>
-      </DialogTrigger>
-      <DialogContent className="w-1/2 h-1/2 p-8">
-       <Image src="/assets/coming-soon.jpeg" fill alt="Profile picture" className="w-20 h-20 hover:scale-100 p-8" />
-       
-      </DialogContent>
-    </Dialog>
+    <div className="absolute w-full h-full z-40">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            variant="outline"
+            className="w-full h-full z-50 overflow-hidden hover:bg-black hover:opacity-20"
+          >
+            Open Modal
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="w-1/2 h-1/2 p-8">
+          <div
+            style={{ padding: "50.83% 0 0 0", position: "relative" }}
+          >
+            <iframe
+              src="https://player.vimeo.com/video/946450529?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+              frameBorder={0}
+              allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+              style={{ position: "absolute", top: "0", left: "0", width: "100%", height: "100%" }}
+              title="RippleEffect.webm"
+            ></iframe>
+          </div>
+          <script src="https://player.vimeo.com/api/player.js"></script>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
+  );
 }
